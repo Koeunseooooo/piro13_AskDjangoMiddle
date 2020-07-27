0e0a32a314737ec086fd98deb317f11b4e3bdb9f
rhdmstj17@cau.ac.kr <rhdmstj17@cau.ac.re>
2020-07-28 00:32:50 +0900

    'unit2'

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -6,5 +6,6 @@ from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
-    path('',lambda req: redirect('/blog/')), #url reverse
+    path('',lambda req: redirect('blog:post_list')), #url reverse
+    path('accounts/',include('accounts.urls'))
 ]
